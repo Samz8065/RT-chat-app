30dff21c525d9692078e3ad6c4a2e213edeff7a3
Samz8065 <samarthk0209@gmail.com>
2025-09-07 00:28:11 +0530

    initial commit

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid email!`
+      },
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength:6
+    },
+    profilePic: {
+      type: String,
+      default: "", 
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
